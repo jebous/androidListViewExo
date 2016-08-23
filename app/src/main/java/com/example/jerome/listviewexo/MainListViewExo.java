4297d077c631b37ae7f1d34102dbc7539b9f1b7a
Jerome <jeromebousquet@free.fr>
2016-08-23 13:55:24 +0200

    Ajout du contextMenu

--- app/src/main/java/com/example/jerome/listviewexo/MainListViewExo.java
+++ app/src/main/java/com/example/jerome/listviewexo/MainListViewExo.java
@@ -2,7 +2,14 @@ package com.example.jerome.listviewexo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,5 +40,35 @@ public class MainListViewExo extends AppCompatActivity {
 
         listView1.setAdapter(adapter);
 
+        registerForContextMenu(listView1);
+
+
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_listview_personne, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.copie:
+                Toast.makeText(getApplicationContext(), "Copie: ID" + menuInfo.id + ", position " + menuInfo.position, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.delete:
+                Toast.makeText(getApplicationContext(), "Delete: ID" + menuInfo.id + ", position " + menuInfo.position, Toast.LENGTH_SHORT).show();
+                PersonneArrayAdapter paa=(PersonneArrayAdapter)listView1.getAdapter();
+                Personne pASupprimer= paa.getItem(menuInfo.position);
+                paa.notifyDataSetChanged();
+                break;
+        }
+        return true;
+    }
+
 }
